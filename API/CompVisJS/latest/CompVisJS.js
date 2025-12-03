053bfbcfbd6e350daa4793e8f4f0f0d0f1163bdb
MathotaGamma <159739543+MathotaGamma@users.noreply.github.com>
2025-12-04 00:42:57 +0900

    Update CompVisJS.js

--- API/CompVisJS/latest/CompVisJS.js
+++ API/CompVisJS/latest/CompVisJS.js
@@ -52,8 +52,38 @@ window.CompVis = class {
   constructor() {
   }
 
-  VectorToQuater(V) {
+  static VectorToQuater(V) {
     return new CompVis.Quater(Vector.values);
+  }
+  
+  static QuaterToMatrix33(q) {
+    const Q = q.clone;
+    const w = Q.w;
+    const x = Q.x;
+    const y = Q.y;
+    const z = Q.z;
+
+    const x2 = x + x;
+    const y2 = y + y;
+    const z2 = z + z;
+    
+    const xx = x * x2;
+    const xy = x * y2;
+    const xz = x * z2;
+    const yy = y * y2;
+    const yz = y * z2;
+    const zz = z * z2;
+    const wx = w * x2;
+    const wy = w * y2;
+    const wz = w * z2;
+
+    const R = [
+        [1 - (yy + zz), xy - wz, xz + wy],
+        [xy + wz, 1 - (xx + zz), yz - wx],
+        [xz - wy, yz + wx, 1 - (xx + yy)]
+    ];
+    
+    return new CompVis.Matrix(R);
   }
 }
 
