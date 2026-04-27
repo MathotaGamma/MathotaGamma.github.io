4ed6ac812deaf75d8efdbc773701dc20e6bff634
MathotaGamma <159739543+MathotaGamma@users.noreply.github.com>
2026-04-27 17:54:29 +0900

    Update main-script.js

--- statics/main-script.js
+++ statics/main-script.js
@@ -43,6 +43,7 @@ async function initBreadcrumb(href=null) {
     splitter: breadcrumb.dataset.colorSplitter ?? "black",
     bg: breadcrumb.dataset.bgColor
   };
+  alert(color.splitter);
 
   // パス分解
   const paths = (href??location.pathname).split("/");
