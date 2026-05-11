c2b69a7395a09832dc8b195939e30f350b80e62a
MathotaGamma <159739543+MathotaGamma@users.noreply.github.com>
2026-05-11 13:05:58 +0900

    Update Calendarer.js

--- APIs/Calendarer/1-01-01/Calendarer.js
+++ APIs/Calendarer/1-01-01/Calendarer.js
@@ -73,15 +73,12 @@
  * @param {boolean} [holidayName=false]
  * true の場合は祝日名を表示
  */
-console.log(76,'hello');
 
 // japanese-holidays-jsとdom-to-image-more(capture内)を使用。
 import JapaneseHolidays from 'https://cdn.jsdelivr.net/npm/japanese-holidays@1.0.10/+esm';
-console.log(80,'loaded');
 
 export default class Calendar {
   constructor(year=null, month=null) {
-    console.log(84,'constructor');
     if (year === null && month === null) this.date = new Date();
     else this.date = Calendar.#getDate(year, month);
   }
@@ -206,7 +203,7 @@ export default class Calendar {
     return info
   }
   
-  /*render(options={}) {
+  render(options={}) {
     if (!this.info) throw new Error("Error: Please run 'getInfo'")
     const info = this.info;
     const width = options.width ?? '90%';
@@ -404,7 +401,7 @@ export default class Calendar {
     link.click();
   }
   
-  async createImage(options_1=null, options_2=null, download=false) {
+  /*async createImage(options_1=null, options_2=null, download=false) {
     if (options_1 === null) options_1 = {};
     if (options_2 === null) options_2 = {};
     
